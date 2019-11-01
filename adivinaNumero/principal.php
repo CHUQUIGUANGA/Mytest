e8201f8294db56af1fbff3ac7f766c0869e5e6c6
ludiscover <dl_chuquiguanga@gmx.com>
2019-11-01 13:44:20 +0100

    ejecuta app

---
+++ adivinaNumero/principal.php
@@ -0,0 +1,14 @@
+<?php 
+$error = $_GET['error'] ?? null;
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Adivina el número</title>
+    </head>
+    <body>
+        <?err ?>
+    </body>
+</html>
